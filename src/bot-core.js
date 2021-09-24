7948c33497ad6d428706e8981787d8205b1da74c
Zyro <timo.22394@hotmail.com>
2021-09-24 11:09:34 -0300

    added some comments
    - added comments to describe the prescence tracker code

--- src/bot-core.js
+++ src/bot-core.js
@@ -1,3 +1,5 @@
+//in this module will be saved the current active users and the time when is has connected to calc the
+//total active time
 module.exports = 
 {
     loggedInUsers: [],
--- src/events/prescenceTracker.js
+++ src/events/prescenceTracker.js
@@ -6,15 +6,20 @@ module.exports =
     once: false,
     execute(oldState, newState, client)
     {
+        //Check if the state is active (active state is connected to a voice channel and not deafned or muted)
         const stateIsActive = (state) => { return (state.channel && !state.deaf && !state.mute) }
 
+        //if the state entered is active
         if(stateIsActive(newState))
         {
+            //register the entered user and the time
             const t = new Date()
             core.loggedInUsers.push( {user: newState.member.user.id, time: t} )
         }
         else
         {
+            //if the state is inactive and the user was registered as active previously,
+            //the register of active user is deleted and the total active time is calculated
             const i = core.loggedInUsers.findIndex(item => item.user == oldState.member.user.id)
 
             if(i != -1)
@@ -22,6 +27,7 @@ module.exports =
                 const item = core.loggedInUsers.splice(i, 1)[0]
                 const time = Math.abs(item.time - new Date())
 
+                //just for testing the time is showed on console
                 console.log(`${item.user} logged out\nSession time: ${time}`)
             }
         }
